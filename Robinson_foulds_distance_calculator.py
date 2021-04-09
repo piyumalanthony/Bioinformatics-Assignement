cf7f1a883d94835b92b0683aafe47e879a8b7d82
Piyumal Anthony <piyumalanthony@gmail.com>
2021-04-09 15:21:56 +0530

    Robinson-foulds distance calculation finalizing

--- Robinson_foulds_distance_calculator.py
+++ Robinson_foulds_distance_calculator.py
@@ -1,16 +1,23 @@
 import dendropy
 from dendropy.calculate import treecompare
 from dendropy import Tree
+import os
+
+protein_dir_set = []
+for i in os.listdir('output'):
+    if "nex" in i.split("."):
+        protein_dir_set.append(str(i))
 
 tns = dendropy.TaxonNamespace()
-tree1 = Tree.get_from_path(
-    "D:\\Code Templates\\Python Templates\\Bioinformatics-Assignement\\output\\efflux_transporter_outer_membrane_subu_tree.nex", "nexus",
-    taxon_namespace=tns)
-tree2 = Tree.get_from_path(
-    "D:\\Code Templates\\Python Templates\\Bioinformatics-Assignement\\output"
-    "\\helix-turn-helix_domain-containing_pro_tree.nex",
-    "nexus", taxon_namespace=tns)
+for i in range(0,len(protein_dir_set)):
+    for j in range(i+1,len(protein_dir_set)):
+        tree1 = Tree.get_from_path(
+            "output/"+ protein_dir_set[i], "nexus",
+            taxon_namespace=tns)
+        tree2 = Tree.get_from_path(
+            "output/"+protein_dir_set[j],
+            "nexus", taxon_namespace=tns)
 
-tree1.encode_bipartitions()
-tree2.encode_bipartitions()
-print(treecompare.weighted_robinson_foulds_distance(tree1, tree2))
+        tree1.encode_bipartitions()
+        tree2.encode_bipartitions()
+        print(protein_dir_set[i], protein_dir_set[j], treecompare.unweighted_robinson_foulds_distance(tree1, tree2))
